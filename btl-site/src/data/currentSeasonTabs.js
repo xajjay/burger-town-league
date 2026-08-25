425bb2b3d9f795138baac8ed4f87af269ee63ee7
xajjay <xajjay13@gmail.com>
2026-08-25 19:49:58 -0400

    restructure

---
+++ btl-site/src/data/currentSeasonTabs.js
@@ -0,0 +1,8 @@
+export const currentSeasonTabs = [
+  { slug: "", label: "Overview" },
+  { slug: "rosters", label: "Rosters" },
+  { slug: "standings", label: "Standings" },
+  { slug: "player-stats", label: "Player Stats" },
+  { slug: "schedule", label: "Schedule" },
+  { slug: "rulebook", label: "Rulebook" },
+];
